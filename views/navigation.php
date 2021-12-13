c8dfb2c2f3ccb599f6f88f907d2a32958970dce3
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2021-12-13 09:35:05 +0100

    added file structure

---
+++ views/navigation.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><?php echo $config['title']; ?></a>
+
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="/index.php">Home</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="/about.php">About</a>
+        </li>
+        <?php if (isset($_SESSION['user'])) : ?>
+            <li class="nav-item">
+                <a class="nav-link" href="/app/users/logout.php">Logout</a>
+            </li>
+        <?php else : ?>
+            <li class="nav-item">
+                <a class="nav-link" href="/login.php">Login</a>
+            </li>
+        <?php
+        endif ?>
+
+    </ul>
+</nav>
