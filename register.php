c38a9508259c89c4537aa28fc7d1c280e34f19bc
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2021-12-15 10:54:29 +0100

    added SOME structure for deleting tasks

--- register.php
+++ register.php
@@ -7,8 +7,8 @@
     <form action="app/users/register.php" method="post">
         <div class="mb-3">
             <label for="email">Email</label>
-            <input class="form-control" type="email" name="email" id="email" placeholder="francis@darjeeling.com" required>
-            <small class="form-text">Please provide the your email address.</small>
+            <input class="form-control" type="email" name="email" id="email" placeholder="email" required>
+            <small class="form-text">Please provide your email address.</small>
         </div>
 
         <div class="mb-3">
