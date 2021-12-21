e31c8e132cf1cb1b4ec4f969c591332a46e3004a
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2021-12-21 11:01:38 +0100

    added upload  img for logged in users

--- app/users/avatar/upload.php
+++ app/users/avatar/upload.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../autoload.php';
+require __DIR__ . '../../../autoload.php';
 
 
-if (isset($_FILES['avatar'])) {
-    move_uploaded_file($_FILES['avatar']['tmp_name'], __DIR__ . './uploads');
+if (isset($_FILES['upload-avatar'])) {
+    $tmp_name = $_FILES['upload-avatar']['tmp_name'];
+    $filetype = pathinfo($_FILES['upload-avatar']['name'], PATHINFO_EXTENSION);
+    $filename = $_SESSION['user']['name'] . '-avatar.' . $filetype;
+    move_uploaded_file($tmp_name, __DIR__ . '../../../../uploads/' . $filename);
 }
--- app/users/register.php
+++ app/users/register.php
@@ -8,16 +8,23 @@ if (isset($_POST['email'], $_POST['username'], $_POST['password'], $_FILES['imag
     $email = trim($_POST['email']);
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
     $username = trim($_POST['username']);
-    $image =
+    $i = 0;
+    $image = $_FILES['image'][$i];
+    $tmp_name = $image['tmp_name'];
+    $filetype = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+    $filename = $username . '-image' . "." . $filetype;
 
-        $statement = $database->prepare('INSERT INTO users
+    move_uploaded_file($tmp_name, __DIR__ . '../../../../uploads/' . $filename);
+
+
+    $statement = $database->prepare('INSERT INTO users
     (user_name, email, password, image_url)
     VALUES
     (:user_name, :email, :password, :image_url)');
     $statement->bindParam(':user_name', $username, PDO::PARAM_STR);
     $statement->bindParam(':email', $email, PDO::PARAM_STR);
     $statement->bindParam(':password', $password, PDO::PARAM_STR);
-    $statement->bindParam(':image_url', $image, PDO::PARAM_STR);
+    $statement->bindParam(':image_url', $filename, PDO::PARAM_STR);
 
 
 
