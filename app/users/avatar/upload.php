53c7db3b1fac3669db8d463368c809cf2a262074
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2021-12-21 12:02:01 +0100

    added db save for uploaded images

--- app/users/avatar/upload.php
+++ app/users/avatar/upload.php
@@ -9,6 +9,16 @@ if (isset($_FILES['image'])) {
     $tmp_name = $_FILES['image']['tmp_name'];
     $filetype = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
     $filename = $_SESSION['user']['id'] . '-avatar.' . $filetype;
+    $user_id = $_SESSION['user']['id'];
     move_uploaded_file($tmp_name, __DIR__ . '../../../../uploads/' . $filename);
+
+    $statement = $database->prepare(
+        'UPDATE users SET (image_url) = (:image_url) WHERE id = :user_id'
+    );
+
+    $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $statement->bindParam(':image_url', $filename, PDO::PARAM_STR);
+    $statement->execute();
+
     redirect('/');
 }
