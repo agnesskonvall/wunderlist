62c8e63073b18283758982f5caedf786c14a4291
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2021-12-22 12:07:40 +0100

    trying to fix lint error

--- app/users/update/password.php
+++ app/users/update/password.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../autoload.php';
+require __DIR__ . '/../../autoload.php';
 
 if (isset($_POST['password'], $_POST['new_password'])) {
     $user_id = $_SESSION['user']['id'];
