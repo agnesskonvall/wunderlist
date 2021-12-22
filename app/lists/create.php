26e4c7f82016a92ecebd3309102cdf5974fed7a1
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2021-12-22 12:35:52 +0100

    fix for lint error

--- app/lists/create.php
+++ app/lists/create.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
-if (isset($_POST['title'],)) {
+if (isset($_POST['title'])) {
     $user_id = $_SESSION['user']['id'];
     $trimmed_title = trim($_POST['title']);
     $title = filter_var($trimmed_title, FILTER_SANITIZE_STRING);
