7374454758af8ff6d93bfc8387e2c0a9ab934038
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2021-12-23 13:03:11 +0100

    added functionality for updating list-names

--- app/lists/update.php
+++ app/lists/update.php
@@ -4,6 +4,22 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
-// In this file we update posts in the database.
+if (isset($_POST['title'])) {
+    $user_id = $_SESSION['user']['id'];
+    $list_id = $_GET['id'];
+    $trimmed_title = trim($_POST['title']);
+    $title = filter_var($trimmed_title, FILTER_SANITIZE_STRING);
 
+    $statement = $database->prepare('UPDATE lists SET
+    title
+    VALUES
+    :title; WHERE id = :id');
+
+    $statement->bindParam(':title', $title, PDO::PARAM_STR);
+    $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $statement->bindParam(':id', $list_id, PDO::PARAM_INT);
+
+
+    $statement->execute();
+}
 redirect('/');
--- individual_list.php
+++ individual_list.php
@@ -14,4 +14,14 @@ foreach ($lists as $list) :
     <?php endif ?>
 <?php endforeach ?>
 
+<form action="/app/lists/update.php?id=<?php $_GET['id'] ?>" method="post">
+    <div class="mb-3">
+        <label for="title">Update list-name</label>
+        <input class="form-control" type="text" name="title" id="title" placeholder="title" required>
+        <small class="form-text">Update the name for your list.</small>
+    </div>
+    <button type="submit">Change title</button>
+
+</form>
+
 <?php require __DIR__ . '/views/footer.php'; ?>
