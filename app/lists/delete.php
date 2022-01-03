e1169850753c008159e6cd0c50b689c0cd7ad900
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2022-01-03 14:55:36 +0100

    adding functionality for daily tasks on index page

--- app/lists/delete.php
+++ app/lists/delete.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
+/// i tried with a innerjoin but just got errors, this is that code: $statement = $database->prepare("DELETE lists, tasks
+///FROM lists INNER JOIN tasks ON tasks.list_id = lists.id WHERE user_id = :user_id AND list.id = :id");
+
+
 if (isset($_GET['id'])) {
     $list_id = $_GET['id'];
     $user_id = $_SESSION['user']['id'];
@@ -13,7 +17,10 @@ if (isset($_GET['id'])) {
     $statement->bindParam(':id', $list_id, PDO::PARAM_INT);
     $statement->execute();
 
-    $_SESSION['messages'][] = 'List deleted from your profile!';
+    $statement = $database->prepare("DELETE FROM tasks WHERE user_id = :user_id AND list_id = :id");
+    $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $statement->bindParam(':id', $list_id, PDO::PARAM_INT);
+    $statement->execute();
 
     redirect('/lists.php');
 }
--- index.php
+++ index.php
@@ -1,10 +1,32 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
+<?php $user_id = $_SESSION['user']['id']; ?>
+
 
 <article>
     <h1><?php echo $config['title']; ?></h1>
     <p>This is the home page.</p>
-
+    <h4>Due today - <?= date('Y-m-d') ?></h4>
+    <ul>
+        <?php
+        $current_tasks = fetch_all_current_tasks($database); ?>
+        <?php if ($current_tasks) : ?>
+            <?php foreach ($current_tasks as $task) : ?>
+                <li>
+                    <h6> <?= $task['title'] ?> </h6>
+                    <a> <?= $task['content'] ?> </a>
+                    <button>
+                        <a href="/individual_task.php?id=<?= $task['id']; ?>">Update </a>
+                    </button>
+                    <button>
+                        <a href="/app/tasks/delete.php?id=<?= $task['id']; ?>">Delete </a>
+                    </button>
+                </li>
+            <?php endforeach ?>
+        <?php else : ?>
+            <h3>Looks like you have the day off!</h3>
+        <?php endif ?>
+    </ul>
 </article>
 
-<?php require __DIR__ . '/views/footer.php'; ?>
+<?php require __DIR__ . '/views/footer.php';
